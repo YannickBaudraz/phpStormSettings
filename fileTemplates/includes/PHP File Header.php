38cef67c6326dae835a9068d99aad5611bc0c1ca
Yannick-Laptop <YannickBaudraz@gmail.com>
2019-01-03 03:49:58 +0100

    PS-2018.3.2 <yanni@Yannick-Laptop Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, project.default.xml, rainbow_brackets.xml, Default copy.xml, other.xml, Default.xml, applicationLibraries.xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, log_highlighting.xml, php.xml, keymap.xml, colors.scheme.xml, databaseDrivers.xml, Default.xml, laf.xml, material_custom_theme.xml, PHP File Header.php, intentionSettings.xml, sexyeditor.xml, material_theme.xml, databaseSettings.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,11 @@
+/**
+ * Created by ${PRODUCT_NAME}.
+ * Author       :   yannick.baudraz@cpnv.ch
+ * Project      :   ${PROJECT_NAME} - ${FILE_NAME}
+ * Description  :   [deescription]
+ * Created      :   ${DATE}
+ * 
+ * Updates      :   [dd.mm.yyyy author]
+ *                  [description]
+ * Git source   :   [link]
+ */
